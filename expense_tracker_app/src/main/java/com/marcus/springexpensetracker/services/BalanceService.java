f799776b3973161772e502a7c2ebe13fbccf6a15
marcusdurairaj <95676434+marcusdurairaj@users.noreply.github.com>
2022-01-29 11:43:16 +0530

    Add files via upload

---
+++ expense_tracker_app/src/main/java/com/marcus/springexpensetracker/services/BalanceService.java
@@ -0,0 +1,8 @@
+package com.marcus.springexpensetracker.services;
+
+import com.sathish.springexpensetracker.models.Balance;
+
+public interface BalanceService {
+	public Balance saveBalance(Balance balance);
+	public Balance createBalance();
+}
